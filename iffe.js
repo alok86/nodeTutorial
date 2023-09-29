7d36a6ab4ee2a8e9ba9e3859c3f52f38879e0f3b
alok seth <alok.seth4@gmail.com>
2023-09-29 17:32:52 +0530

    12/64ModuleWrapper

--- iffe.js
+++ iffe.js
@@ -1,20 +1,22 @@
-(function () {
+(function (message) {
   const superHero = 'Batman';
-  console.log(superHero);
-})();
+  console.log(message, superHero);
+})('hello');
 
-(function () {
+(function (message) {
   const superHero = 'superman';
-  console.log(superHero);
-})();
+  console.log(message, superHero);
+})('hey');
 
 // here each function has ther own private scope
 //Before a module's code is executed , Node.js
 //will wrap it with a function wrapper that provides
 // module scope
 /**
- * (function(){
+ * (function(exports , require , module , __filename, __dirname){
  * module code actully lives in here
  * }
- * )
+ * )(parameter string)
+ *
+ * like this iffe index.js calling the module
  */
