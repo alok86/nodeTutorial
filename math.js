db2a266a90563269a27c17fde73ebeadb3dbe340
alok seth <alok.seth4@gmail.com>
2023-10-01 20:45:01 +0530

    14/64 module exports pattern 5

--- math.js
+++ math.js
@@ -1,7 +1,12 @@
-module.exports.add = (a, b) => {
+exports.add = (a, b) => {
   return a + b;
 };
 
-module.exports.subtract = (a, b) => {
+exports.subtract = (a, b) => {
   return a - b;
 };
+
+/*
+if it is remember iffe has five parameter in which 
+exports and module ref is there therefore only exports is enough
+*/
