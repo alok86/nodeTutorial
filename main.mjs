641f9560e391de9c36d681794edf9d533c530618
alok seth <alok.seth4@gmail.com>
2023-10-01 22:22:18 +0530

    15/64 es module 6 importing 2

--- main.mjs
+++ main.mjs
@@ -1,4 +1,4 @@
-import * as cal from './math-esm.mjs';
-const { add, subtract } = cal;
+import { add, subtract } from './math-esm.mjs';
+//const { add, subtract } = cal;
 console.log(add(3, 5));
 console.log(subtract(5, 3));
