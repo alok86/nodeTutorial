2d031c81e72beabebd9ed3b0c5294e708b09f08f
alok seth <alok.seth4@gmail.com>
2023-10-01 20:16:12 +0530

    14/64 module export pattern 1

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const SuperHero = require('./super-hero');
-
-const batman = new SuperHero('Batman');
-console.log(batman.getName());
-batman.setName('Bruce Wayne');
-console.log(batman.getName());
-
-const superman = new SuperHero('Superman');
-console.log(superman.getName());
+const add = require('./math');
+console.log(add(3, 8));
