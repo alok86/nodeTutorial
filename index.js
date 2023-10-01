18d588e3c8a546fe9e509662886a8191ba1d806a
alok seth <alok.seth4@gmail.com>
2023-10-01 19:32:22 +0530

    13/64 module caching

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const superHero = require('./super-hero');
+console.log(superHero.getName());
+superHero.setName('Superman');
+console.log(superHero.getName());
+
+const newSuperHero = require('./super-hero');
+console.log(newSuperHero.getName());
+// and output of above line is Superman this is called module caching
+// and instead of loading fresh module it loaded the previously loaded module
+
 // even the both module have same code and variable
 // still there is no conflict because each module has
 // there own scope
-require('./fighter');
-require('./warrior');
+//require('./fighter');
+//require('./warrior');
 
 // const add = require('./add');
 
