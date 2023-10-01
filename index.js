0aa3294f4a0b43339f9fcaa0a8adece4d1f65d72
alok seth <alok.seth4@gmail.com>
2023-10-01 19:56:41 +0530

    13/64Class Export

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const superHero = require('./super-hero');
-console.log(superHero.getName());
-superHero.setName('Superman');
-console.log(superHero.getName());
+const SuperHero = require('./super-hero');
 
-const newSuperHero = require('./super-hero');
-console.log(newSuperHero.getName());
-// and output of above line is Superman this is called module caching
-// and instead of loading fresh module it loaded the previously loaded module
+const batman = new SuperHero('Batman');
+console.log(batman.getName());
+batman.setName('Bruce Wayne');
+console.log(batman.getName());
 
-// even the both module have same code and variable
-// still there is no conflict because each module has
-// there own scope
-//require('./fighter');
-//require('./warrior');
-
-// const add = require('./add');
-
-// console.log('hello , node js');
-// const sum = add(4, 6);
-// console.log(sum);
+const superman = new SuperHero('Superman');
+console.log(superman.getName());
