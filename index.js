e8589bf32e16d7469f4332699f693011b22369b3
alok seth <alok.seth4@gmail.com>
2023-10-02 19:51:58 +0530

    19/64 builtin module Path Join

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const path = require('node:path');
 
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
 
-console.log(path.extname(__filename));
-console.log(path.extname(__dirname));
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
 
-console.log(path.parse(__filename));
+// console.log(path.parse(__filename));
 
-console.log(path.format(path.parse(__filename)));
-console.log(path.isAbsolute(__filename));
+// console.log(path.format(path.parse(__filename)));
+// console.log(path.isAbsolute(__filename));
 
-console.log(path.isAbsolute('./data.json'));
+// console.log(path.isAbsolute('./data.json'));
+console.log(path.join('folder1', 'folder 2', 'index.html'));
+console.log(path.join('/folder1', 'folder 2', 'index.html'));
+console.log(path.join('folder1', '//folder 2', 'index.html'));
+console.log(path.join('/folder1', '//folder 2', '../index.html'));
+console.log(path.join(__dirname, 'data.json'));
