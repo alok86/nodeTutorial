7d37d59f76283161135637f417c51cb3ff60bc77
alok seth <alok.seth4@gmail.com>
2023-10-02 21:22:09 +0530

    21/64 event module

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-function greet(name) {
-  console.log(`hello ${name}`);
-}
+const EventEmmiter = require('node:events');
 
-function greetToMe(greetFn) {
-  const name = 'developer';
-  greetFn(name);
-  //and above function call is call back function
-}
-// above function is higher order function
-// because it is calling to another function
-greetToMe(greet);
+const emmiter = new EventEmmiter();
 
-// any function call to another function called
-// call back function
+emmiter.on('order-pizza', (size, topping) => {
+  console.log(`order recived ! baking a ${size} pizza with ${topping}`);
+});
 
-// Synchronous call back function call immeditately when
-// control goes to higher order function
+// you can add multiple listner
 
-// Asynchronus call back
+emmiter.on('order-pizza', (size) => {
+  if (size === 'large') {
+    console.log('serving complimentary drink');
+  }
+});
 
-// A Call back that is often used to continue or resume
-// code execution after an asynchronous operation has completed
-
-// callback are used to delay the execution of a function until
-// a particulat time of event has occurred
-
-// in node.js have an asynchronous to prevent
-// blocking of execution
-// ex - reading data from file , fetching data from
-// a database or hadling a network request
+emmiter.emit('order-pizza', 'large', 'mushroom');
