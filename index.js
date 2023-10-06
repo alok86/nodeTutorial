032c159af9947264367c8393b338faef3e08bbba
alok seth <alok.seth4@gmail.com>
2023-10-06 19:44:24 +0530

    24/64 stream and buffer

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const PizzaShop = require('./pizza-shop');
-const DrinkMachine = require('./drink-machine');
+const buffer = new Buffer.from('aalok');
 
-const pizza = new PizzaShop();
-const drink = new DrinkMachine();
+/**
+ * As buffer has limited memory therefore it
+ * will replace aalo from aalok and place Code
+ * over there and form Codek
+ */
+buffer.write('Code');
 
-pizza.on('order', (size, toppings) => {
-  console.log(`Order reciverd ! Baking a ${size} pizza with ${toppings}`);
-  drink.serveDrink(size);
-});
+console.log(buffer.toString());
+/*
+ * if we print the buffer  directly it will return the
+ * hexadecimal value of each word individually
+ * for example out put of following console log
+ * will be <Buffer 61 61 6c 6f 6b> which is
+ * hexadecimal equivalant of 97,97,108,111,107
+ */
+console.log(buffer);
 
-pizza.order('large', 'mushrooms');
-pizza.displayOrderNumber();
-// const EventEmmiter = require('node:events');
-
-// const emmiter = new EventEmmiter();
-
-// emmiter.on('order-pizza', (size, topping) => {
-//   console.log(`order recived ! baking a ${size} pizza with ${topping}`);
-// });
-
-// // you can add multiple listner
-
-// emmiter.on('order-pizza', (size) => {
-//   if (size === 'large') {
-//     console.log('serving complimentary drink');
-//   }
-// });
-
-// emmiter.emit('order-pizza', 'large', 'mushroom');
+/*
+ * like the above code following code will also
+ * genereate data encoding but in assci equivelent of
+ * of chatecter like
+ * 97 ,97 ,108 , 111, 107 in data segment
+ */
+console.log(buffer.toJSON());
---
+++ readme.md
@@ -0,0 +1,7 @@
+23/64 - topics are
+1 character sets
+2 character Encoding
+3 Stream and buffers
+4 Asynchrouns javaScript
+note - > at bowser console "v".charCodeAt() return 86
+24/64 - stream and buffer
